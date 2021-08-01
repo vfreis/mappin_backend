c9ee9dc128e4e84a31faafb259fab4675123b5a9
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-08-01 15:37:25 -0300

    mappin backend for production

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const app = express()
+const userRoute = require('./routes/users')
+const pinRoute = require('./routes/pins')
+
+dotenv.config()
+
+app.use(express.json())
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(()=>{
+        console.log('MongoDB is connected')
+    })
+    .catch((err)=> console.log(err))
+
+app.use('/api/users', userRoute)
+app.use('/api/pins', pinRoute)
+
+app.listen(process.env.PORT || 8800, ()=>{
+    console.log('Backend server is running!')
+})
